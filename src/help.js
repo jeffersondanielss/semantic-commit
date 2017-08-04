f7996ade9a88841c23c9b9351c8d39b864f155a8
Jefferson Daniel <jeffersondanielss@gmail.com>
2017-08-03 23:12:08 -0300

    docs: add help for wrong commands

---
+++ src/help.js
@@ -0,0 +1,23 @@
+const help = () => console.log(`
+  - SEMANTIC COMMIT -
+  --------------------------------------------------------------------------  
+
+  I use a rigid commit message format, and it makes me a better programmer.
+
+  feat: add hat wobble
+  ^--^  ^------------^
+  |     |
+  |     +-> Summary in present tense.
+  |
+  +-------> Type: chore, docs, feat, fix, refactor, style, or test.
+
+  To do so after adding your files to stash use the semantic-commit
+  command to standardize your commits.
+
+  Commands
+    - semantic-commit
+    - semantic-commit --help
+
+`)
+
+module.exports = help
