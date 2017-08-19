4d09358d63026e260c2edc449b00b2ed599c8510
Jefferson Daniel <jeffersondanielss@gmail.com>
2017-08-19 11:55:19 -0300

    test: return version

--- test.js
+++ test.js
@@ -1,35 +1,46 @@
 const test = require('ava')
-const questions = require('./src/questions')
-const getCommit = require('./src/getCommit')
+const path = require('path')
 const { exec } = require('child_process')
+const pkg = require(path.join(__dirname, './package.json'))
+
 const help = require('./src/help')
+const {pt, en} = require('./src/questions')
+const getCommit = require('./src/getCommit')
 
 test('Nome da lista de commits', t => {
-  let message = questions[0].name
+  let message = en[0].name
+  let messagePt = pt[0].name
   let result = 'type'
   
   t.is(message, result);
+  t.is(messagePt, result);
 });
 
 test('Lista de opções de commits', t => {
-  let message = questions[0].type
+  let message = en[0].type
+  let messagePt = pt[0].type
   let result = 'list'
   
   t.is(message, result);
+  t.is(messagePt, result);
 });
 
 test('formato de escolhas dos commits', t => {
-  let message = Array.isArray(questions[0].choices)
+  let message = Array.isArray(en[0].choices)
+  let messagePt = Array.isArray(pt[0].choices)
   let result = true
   
   t.is(message, result);
+  t.is(messagePt, result);
 });
 
 test('Nome do input que recebe a mensagem do commit', t => {
-  let message = questions[1].name
+  let message = en[1].name
+  let messagePt = pt[1].name
   let result = 'message'
   
   t.is(message, result);
+  t.is(messagePt, result);
 });
 
 test('Retornando tipo e mensagem do usuário em um comando', t => {
@@ -39,6 +50,11 @@ test('Retornando tipo e mensagem do usuário em um comando', t => {
   t.is(message, result);
 });
 
-test.todo('Retorna versão do projeto')
-test.todo('Retorna ajuda caso usuário digite um comando inválido')
-test.todo('Retorna ajuda caso usuário digite o comando --help')
+test('Retorna versão do projeto', t => {
+  let version = require('./src/getVersion')()
+  let result = pkg.version
+
+  t.is(version, result);
+})
+
+test.todo('Filtro de parâmetros passados')
