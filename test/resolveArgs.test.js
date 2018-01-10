ddf5c6e2b8ec5336a17c46fe3f4ce037f7671cb1
Jefferson Daniel <jdsilva@sp.r7.com>
2018-01-10 14:56:25 -0200

    teste: aumentando cobertura na função resolveArgs

--- test/resolveArgs.test.js
+++ test/resolveArgs.test.js
@@ -1,9 +1,33 @@
 const test = require('ava')
 const { resolveArgs } = require('../lib/resolveArgs')
+const getVersion = require('../lib/getVersion')
+const help = require('../lib/help')
 
-test('resolveArgs é uma função', t => {
+
+test('é uma função', t => {
   let fn = typeof resolveArgs
   let result = 'function'
+
+  t.is(fn, result);
+})
+
+test('recebendo argumento "-v" exibe a versão da lib', async t => {
+  let fn = await resolveArgs('-v')
+  let result = await getVersion();
+
+  t.is(fn, result);
+})
+
+test('recebendo argumento "-h" exibe ajuda', async t => {
+  let fn = await resolveArgs('-h')
+  let result = await help();
+
+  t.is(fn, result);
+})
+
+test('recebendo um argumento não existente exibe ajuda', async t => {
+  let fn = await resolveArgs('--argumentoErrado')
+  let result = await help();
 
   t.is(fn, result);
 })
